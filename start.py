eed7c7b75c5eb92cca9386f2d50bfa2203082d3d
Dhai Eddine Zebbiche <joseph.zebbiche@gmail.com>
2023-06-11 16:31:15 +0200

    temp commit

--- start.py
+++ start.py
@@ -5,24 +5,36 @@ print("[*] Start very large scenario...")
 
 env = gym.make("mobile-verylarge-central-v0", render_mode="rgb_array")
 
+obs, info = env.reset()
 
 print("[+] Number of InPs: ", env.NUM_InPs)
 print("[+] Number of service providers: ", env.NUM_SPs)
 
 print(
-    f"Very large environment with:\
+    f"\nVery large environment with:\
     \n* {env.NUM_USERS} users\
     \n* {env.NUM_STATIONS} base stations\
     \n* {env.NUM_EDGE_SERVERS} edge servers"
 )
 
-for bs in env.stations:
-    for es in env.edge_servers:
-        continue
+print("\n[*] InPs offered bundles:")
+for inp in env.inps:
+    print(f"     InP: {inp.inp_id}, number of edge servers: {inp.offer_bundle()}")
 
-obs, info = env.reset()
+print("\n[*] Edge servers per InP:")
+for inp in env.inps:
+    print(f"     InP: {inp.inp_id}, number of edge servers: {len(inp.edge_servers)}")
 
-TIME = 6  # timestamps
+print("\n[*] SPs' budgets and pricing:")
+for sp in env.sps:
+    print(f"     Initial budget: {sp.Budget} $, subscription fee: {sp.subscription_fee} $")
+
+print("\n[*] Users per SP:")
+for sp in env.sps:
+    print(f"     SP: {sp.sp_id}, number of users: {len(sp.users)}")
+
+
+TIME = 0  # timestamps
 for t in range(TIME):
     # send tasks for corresponding service providers / network states
     # Sps bid for InPs ==> action space
