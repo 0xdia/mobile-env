caf4a27fe99b1b63158482ac113803898722fc0d
Dhai Eddine Zebbiche <id_zebbiche@esi.dz>
2023-06-14 16:27:55 +0200

    add net-states to state space

--- mobile_env/handlers/central.py
+++ mobile_env/handlers/central.py
@@ -30,6 +30,9 @@ class MComCentralHandler(Handler):
             {
                 "bundles": spaces.Box(low=0, high=99999, shape=(10, 3), dtype=np.int32),
                 "tasks": spaces.Box(low=0, high=850, shape=(815, 4), dtype=np.int32),
+                "net-states": spaces.Box(
+                   low=0, high=850, shape=(815 * 124, 3), dtype=np.int32
+                ),
             }
         )
 
